7262aa5a2171f2a7ddf3643eddfa16e1c0ece5d4
aarushh007 <aarushh007@gmail.com>
2021-04-19 17:22:58 -0400

    routing changes

--- 11-http.js
+++ 11-http.js
@@ -2,20 +2,23 @@ const http = require('http');
 
 const server = http.createServer((req, res)=>{
     console.log('I got a request on ', req.url)
-    if(req.url === '/'){
-        res.write('hello engineers')
-        res.end();
-    }else 
-    if(req.url === '/about'){
-        res.write('About page')
-        res.end();
-    } else {
-    res.write(`
-    <h1>Whoops!</h1>
-    <h3>Page not found</h3>
-    <a href='/'>Go back to home page</a>
-    `)
-    res.end()
+    switch(req.url){
+        case '/':
+            res.write('hello engineers');
+            res.end();
+            break;
+        case '/about':
+            res.write('About page');
+            res.end();
+            break;
+        default:
+            res.write(`
+                <h1>Whoops!</h1>
+                <h3>Page not found</h3>
+                <a href='/'>Go back to home page</a>
+            `)
+            res.end();
+            break;
     }
 })
 

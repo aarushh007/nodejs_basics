954ad142bd38dccf4d9b0e8af97093fa364042bc
aarushh007 <aarushh007@gmail.com>
2021-04-21 11:06:08 -0400

    idk

--- 12-packages.js
+++ 12-packages.js
@@ -1,6 +1,6 @@
 const _ = require('lodash');
 
-const items = [1, [2, [3, [4, [5, [6]]]]]];
+const items = [1, [2, [3, [4, [5, [6, [7, [8]]]]]]]];
 
 const flattened = _.flattenDeep(items);
 console.log(flattened)
